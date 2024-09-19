2c0a01a5356bea32728466f36480157bb9eeeff4
Arpita Barode_07 <100395290+ArpitaBarode0702@users.noreply.github.com>
2024-09-19 19:49:04 +0530

    Time: 13 ms (20.38%) | Memory: 45 MB (90.54%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,16 +1,20 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       HashMap<Integer,Integer> map=new HashMap<>();
-        int i=0;
-        while(i<nums.length){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            i++;
+       HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Count the frequency of each number
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        for(Map.Entry<Integer,Integer> it:map.entrySet()){
-            if(it.getValue()==1){
-                return it.getKey();
+        
+        // Find the number that appears only once
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
             }
         }
+        
         return -1;
+        
     }
 }
